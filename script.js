b099344d27955d9995aa0d1d7dc48e712e2ed1a7
Bithi-84 <tehzeebbithi.2016@gmail.com>
2024-10-29 23:06:41 +0600

    js function

---
+++ script.js
@@ -0,0 +1,44 @@
+
+
+function sum  ( number1, number2)
+{
+    let x = number1;
+    let y = number2;
+    let result = x+y;
+
+    // console.log(result);
+    return result;
+
+}
+
+// console.log(sum (54, 90)); 144
+
+let summation = sum(100, 300); //400
+
+console.log(summation);
+
+let secondsum = summation + 120;
+console.log(secondsum);
+
+sum  (20, 56);
+
+let thirdsum = sum (20, 56);
+console.log (thirdsum);
+
+
+function sub ( number1, number2)
+
+{
+    let x = number1;
+    let y = number2;
+
+    let result = number1- number2;
+     return result;
+
+
+}
+
+let subtraction = sub (100,38);
+// return result;
+
+console.log(subtraction);
